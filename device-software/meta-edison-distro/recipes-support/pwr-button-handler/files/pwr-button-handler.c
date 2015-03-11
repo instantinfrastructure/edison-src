5e6e72510a225de2cbd6fe681ec6b1ec0133f54c
Chris McClimans <chris@hippiehacker.org>
2015-03-11 15:37:27 -0700

    powerbutton service to blink-led for onetimesetup

--- device-software/meta-edison-distro/recipes-support/pwr-button-handler/files/pwr-button-handler.c
+++ device-software/meta-edison-distro/recipes-support/pwr-button-handler/files/pwr-button-handler.c
@@ -46,8 +46,6 @@
 /* We use 2 seconds for now */
 #define EDISON_OOBE_PRESS_TIMEOUT 2
 
-#define CALLED_COMMAND_ON_TIMEOUT "/usr/bin/configure_edison --enableOneTimeSetup"
-
 struct input_event {
     struct timeval time;
     unsigned short type;
@@ -64,9 +62,21 @@ int main(int argc, char **argv)
     int fd, n;
     ssize_t len;
 
+    if (argc!=3)
+    {
+        printf("Usage:\n");
+        printf("  %s \"command_line1\" \"command_line2\"\n", argv[0]);
+        printf("     command_line1: a command line to execute when the PWR button is pressed for more than 2s\n");
+        printf("     command_line2: a second command line to execute when the PWR button is released (after being pressed for more than 2s)\n");
+        return -1;
+    }
+
+
     /* Time of the last press event.
        We reset this to zero when the button is released */
     time_t time_at_last_press = 0;
+
+    int event_already_fired = 0;
 
     fd = open("/dev/input/event1", O_RDONLY);
     if (fd < 0) {
@@ -79,14 +89,24 @@ int main(int argc, char **argv)
     p.events = POLLIN;
 
     while (1) {
-        n = poll(&p, 1, -1);
+        /* Refresh every 20 ms if the user already started pressing the button */
+        n = poll(&p, 1, time_at_last_press==0 ? -1 : 20);
         if (n < 0) {
             perror("Failed to poll /dev/input/event1 device");
             break;
         }
-
-        if (n == 0)
+        if (n==0) {
+            /* Poll timed out */
+            gettimeofday(&tv, NULL);
+            if (tv.tv_sec - time_at_last_press >= EDISON_OOBE_PRESS_TIMEOUT && event_already_fired == 0)
+            {
+                event_already_fired = 1;
+                printf("Edison PWR button was pressed more than 2s\n");
+                fflush(stdout);
+                system(argv[1]);
+            }
             continue;
+        }
 
         len = read(fd, &event, sizeof(event));
         if (len < 0) {
@@ -99,7 +119,7 @@ int main(int argc, char **argv)
             break;
         }
 
-        /* ignore non KEY event, and non RM button events */
+        /* ignore non KEY event, and non PWR button events */
         if (event.type != EV_KEY || event.code != KEY_POWER)
             continue;
 
@@ -116,6 +136,7 @@ int main(int argc, char **argv)
         {
             case 1: /* Regular press */
                 assert(time_at_last_press==0);
+                assert(event_already_fired==0);
                 time_at_last_press = tv.tv_sec;
                 break;
             case 2: /* Auto repeat press */
@@ -126,14 +147,14 @@ int main(int argc, char **argv)
                 }
                 break;
             case 0: /* Release */
-                if (tv.tv_sec - time_at_last_press >= EDISON_OOBE_PRESS_TIMEOUT)
+                if (event_already_fired != 0)
                 {
-                    time_at_last_press = 0;
-                    printf("Edison PWR button was pressed more than 2s, starting OOBE service..\n");
+                    printf("Edison PWR button was pressed more than 2s and released\n");
                     fflush(stdout);
-                    system(CALLED_COMMAND_ON_TIMEOUT);
+                    system(argv[2]);
                 }
                 time_at_last_press = 0;
+                event_already_fired = 0;
                 break;
             default:
                 printf("Warning: unhandled PWR button event value: %u\n", event.value);
